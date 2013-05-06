345e54210e0860097627f21468e31965c9d66776
Ishan Jetley <ishanjet@usc.edu>
2013-05-05 17:26:12 -0700

    Removed User Id

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -34,11 +34,22 @@ class StatusesControllerTest < ActionController::TestCase
 
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
-    end
+  end
 
     assert_redirected_to status_path(assigns(:status))
   end
 
+
+  test "should create status for the current user when logged in" do
+    sign_in users(:jason)
+
+    assert_difference('Status.count') do
+      post :create, status: { content: @status.content, user_id: users(:jim).id }
+  end
+
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_idm users(:jason).id
+  end
   test "should show status" do
     get :show, id: @status
     assert_response :success
@@ -66,6 +77,13 @@ class StatusesControllerTest < ActionController::TestCase
     sign_in users(:jason)
     put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
+  end
+
+   test "should update status when current user is logged in" do
+    sign_in users(:jason)
+    put :update, id: @status, status: { content: @status.content, user_id: users (:jim).id }
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:jason).id
   end
 
   test "should destroy status" do
